39fa9e9d1949d18503f0e9ee3506079b28d236a0
yangming <yangming21_ok@163.com>
2020-07-21 19:24:15 +0800

    update

--- demo/src/main/java/com/ym/learn/demo/designpatterns/shangguiguhanshunping/patterns/adapter/interfaceadapter/Client.java
+++ demo/src/main/java/com/ym/learn/demo/designpatterns/shangguiguhanshunping/patterns/adapter/interfaceadapter/Client.java
@@ -2,6 +2,7 @@ package com.ym.learn.demo.designpatterns.shangguiguhanshunping.patterns.adapter.
 
 public class Client {
     public static void main(String[] args) {
+
         //使用匿名内部类的方式实现接口适配器
         AbsAdapter adapter = new AbsAdapter() {
             //只需要覆盖 需要使用的接口方法
