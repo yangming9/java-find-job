0c3f90f66cea752e123bfb696771bd374f2adf0a
yangming <yangming21_ok@163.com>
2020-07-21 09:43:49 +0800

    完善简单工厂模式

--- demo/src/main/java/com/ym/learn/demo/designpatterns/shangguiguhanshunping/patterns/factory/old/order/PizzaStore.java
+++ demo/src/main/java/com/ym/learn/demo/designpatterns/shangguiguhanshunping/patterns/factory/old/order/PizzaStore.java
@@ -1,10 +1,12 @@
-package com.ym.learn.demo.designpatterns.shangguiguhanshunping.patterns.factory.simplefactory.order;
+package com.ym.learn.demo.designpatterns.shangguiguhanshunping.patterns.factory.old.order;
+
+import com.ym.learn.demo.designpatterns.shangguiguhanshunping.patterns.factory.simplefactory.SimpleFactory;
 
 /**
  * 相当于一个客户端  发起订购任务
  */
 public class PizzaStore {
     public static void main(String[] args) {
-        new OrderPizza();
+        new OrderPizza(new SimpleFactory());
     }
 }
