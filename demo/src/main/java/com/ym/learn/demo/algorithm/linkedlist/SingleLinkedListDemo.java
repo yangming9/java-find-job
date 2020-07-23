48cc10bbef1a66912e9dc9b6870e6666376570f9
yangming <yangming21_ok@163.com>
2020-07-23 21:03:30 +0800

    增加  反转单链表  和 查找链表中 倒数第几个节点

--- demo/src/main/java/com/ym/learn/demo/algorithm/linkedlist/SingleLinkedListDemo.java
+++ demo/src/main/java/com/ym/learn/demo/algorithm/linkedlist/SingleLinkedListDemo.java
@@ -20,18 +20,85 @@ public class SingleLinkedListDemo {
         System.out.println("-------删除指定节点-------");
         singleLinkedList.delNode(5);
         singleLinkedList.list();
-        System.out.println("链表的节点个数为："+getLinkedListLength(singleLinkedList.getHead()));
+        System.out.println("链表的节点个数为：" + getLinkedListLength(singleLinkedList.getHead()));
+        System.out.println(getHeroNodeByIndex(singleLinkedList.getHead(),2).toString());
+        System.out.println("-----链表反转-----");
+        reverseLinkedList(singleLinkedList);
+        singleLinkedList.list();
+    }
+
+    /**
+     * 反转单链表
+     */
+    public static void reverseLinkedList(SingleLinkedList singleLinkedList){
+        HeroNode head = singleLinkedList.getHead();
+        //链表为空或只有一个节点无需反转
+        if (head.next == null || head.next.next == null){
+            return;
+        }
+
+        //定义辅助指针 遍历原来的链表
+        HeroNode cur = head.next;
+        HeroNode next = null;//指向当前节点的下一个节点
+
+        HeroNode reverseHead = new HeroNode(0,"","");
+
+        while (cur !=null){
+            next = cur.next;//暂时保存当前节点的下一个节点
+            cur.next = reverseHead.next;
+            reverseHead.next = cur;
+            cur = next;
+        }
+
+        head.next = reverseHead.next;
+    }
+
+    /**
+     * 查找单链表中 倒数第k个节点
+     * 思路
+     * 1.编写一个方法接收head节点 同时接收一个index
+     * 2.index表示倒数第index个节点
+     * 3.先将链表遍历 得到链表总长度
+     * 4.得到size后，从链表第一个开始遍历（size-index）ge 就可以得到
+     *
+     * @param head
+     * @param index
+     * @return
+     */
+    public static HeroNode getHeroNodeByIndex(HeroNode head, int index) {
+        if (head.next == null || index < 0) {
+            return null;
+        }
+
+        int length = getLinkedListLength(head);
+
+        if (index > length){
+            return null;
+        }
+
+        HeroNode cur = head.next;
+        int i = 0;
+        while (i<length-index){
+            cur = cur.next;
+            i++;
+        }
+        return cur;
 
     }
 
-    //获取链表的个数  如果带头结点 的链表 需要将头结点去掉 不进行统计
-    public static int getLinkedListLength(HeroNode node){
-        if (node.next == null){
+    /**
+     * 面试题目  查找链表中节点的个数  不包括头结点
+     *
+     * @param head
+     * @return
+     */
+    public static int getLinkedListLength(HeroNode head) {
+        if (head.next == null) {
             return 0;
         }
         int length = 0;
-        HeroNode temp = node.next;
-        while (temp != null){
+        HeroNode temp = head.next;
+        while (temp != null) {
             length++;
             temp = temp.next;
         }
@@ -45,7 +112,7 @@ class SingleLinkedList {
     //初始化一个头结点   头结点一般不要动
     private HeroNode head = new HeroNode(0, "", "");
 
-    public HeroNode getHead(){
+    public HeroNode getHead() {
         return head;
     }
 
@@ -94,11 +161,12 @@ class SingleLinkedList {
     /**
      * 找到需要删除的节点的上一个节点
      * 被删除的节点 不会存在其他引用 会被垃圾回收机制回收
+     *
      * @param no
      */
-    public void delNode(int no){
+    public void delNode(int no) {
         //判断链表是否为空
-        if (head.next == null){
+        if (head.next == null) {
             System.out.println("链表为空");
         }
 
@@ -106,20 +174,20 @@ class SingleLinkedList {
 
         boolean flag = false;
 
-        while (true){
-            if (temp.next == null){
+        while (true) {
+            if (temp.next == null) {
                 break;
             }
-            if (temp.next.no == no){
+            if (temp.next.no == no) {
                 flag = true;
                 break;
             }
             temp = temp.next;
         }
-        if (flag){
+        if (flag) {
             temp.next = temp.next.next;
-        }else {
-            System.out.println("需要删除的节点不存在 no="+no);
+        } else {
+            System.out.println("需要删除的节点不存在 no=" + no);
         }
 
     }
@@ -157,7 +225,7 @@ class SingleLinkedList {
     public void list() {
         //判断链表是否为空
         if (head.next == null) {
-            System.out.println("链表为空");
+            System.out.println("======链表为空");
             return;
         }
         HeroNode temp = head.next;
