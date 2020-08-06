397103a3de0adb66ec6e1568d95cea3ba92f092a
yangming <yangming21_ok@163.com>
2020-08-06 10:09:14 +0800

    update

--- demo/src/main/java/com/ym/learn/demo/designpatterns/shangguiguhanshunping/patterns/strategy/tradition/Client.java
+++ demo/src/main/java/com/ym/learn/demo/designpatterns/shangguiguhanshunping/patterns/strategy/tradition/Client.java
@@ -2,6 +2,11 @@ package com.ym.learn.demo.designpatterns.shangguiguhanshunping.patterns.strategy
 
 /**
  * 策略模式
+ * 传统的实现   缺点就在于  子类 必须重写不符合的父类的方法
+ * 根源在于继承 多使用基于接口编程和聚合
  */
 public class Client {
+    public static void main(String[] args) {
+
+    }
 }
