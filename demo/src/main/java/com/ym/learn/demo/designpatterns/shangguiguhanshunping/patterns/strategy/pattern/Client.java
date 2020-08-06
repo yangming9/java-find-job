795a3da1ab8c1b521beee3428c387d99d82d3af0
yangming <yangming21_ok@163.com>
2020-08-06 14:50:38 +0800

    update

--- demo/src/main/java/com/ym/learn/demo/designpatterns/shangguiguhanshunping/patterns/strategy/pattern/Client.java
+++ demo/src/main/java/com/ym/learn/demo/designpatterns/shangguiguhanshunping/patterns/strategy/pattern/Client.java
@@ -1,5 +1,8 @@
 package com.ym.learn.demo.designpatterns.shangguiguhanshunping.patterns.strategy.pattern;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * 策略模式  真正的设计模式
  */
@@ -17,5 +20,21 @@ public class Client {
         pekingDuck.display();
         pekingDuck.fly();
         pekingDuck.quack();
+
+
+        Integer[] data = {9, 1, 2, 8, 3, 4};
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (o1 > o2) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        };
+
+        Arrays.sort(data,comparator);
+        System.out.println(Arrays.toString(data));
     }
 }
