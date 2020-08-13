beac82dd84b97e17299a2bc074d6295207412b05
ym <yangming21_ok@13.com>
2020-08-13 16:01:50 +0800

    增加二分查找的升级版本
    对于部分倒叙的序列进行排序

--- demo/src/main/java/com/ym/learn/demo/Test.java
+++ demo/src/main/java/com/ym/learn/demo/Test.java
@@ -2,7 +2,6 @@ package com.ym.learn.demo;
 
 public class Test {
     public static void main(String[] args) {
-
     }
 }
 
--- demo/src/main/java/com/ym/learn/demo/redis/IndexController.java
+++ demo/src/main/java/com/ym/learn/demo/redis/IndexController.java
@@ -1,20 +1,17 @@
 package com.ym.learn.demo.redis;
 
-import org.redisson.Redisson;
-import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 public class IndexController {
 
-    @Autowired
-    private Redisson redisson;
+//    @Autowired
+//    private Redisson redisson;
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
@@ -25,7 +22,7 @@ public class IndexController {
 
         String clientId = UUID.randomUUID().toString().replace(".", "-");
         System.out.println(clientId);
-        Boolean result = stringRedisTemplate.opsForValue().setIfAbsent("lock", clientId, 10, TimeUnit.SECONDS);
+        Boolean result = stringRedisTemplate.opsForValue().setIfAbsent("lock", clientId);
 
         if (!result) {
             return "error";
@@ -51,27 +48,27 @@ public class IndexController {
         return "end";
     }
 
-    @RequestMapping("/deductStock2")
-    public String deductStock2() throws InterruptedException {
-        RLock lock = redisson.getLock("lock");
-
-        try {
-
-            lock.lock();
-
-            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
-            if (stock > 0) {
-                int realStock = stock - 1;
-                stringRedisTemplate.opsForValue().set("stock", realStock + "");
-                System.out.println("扣减成功，剩余库存：" + realStock);
-            } else {
-                System.out.println("扣减失败，库存不足");
-            }
-        } finally {
-            lock.unlock();
-        }
-
-
-        return "end";
-    }
+//    @RequestMapping("/deductStock2")
+//    public String deductStock2() throws InterruptedException {
+//        RLock lock = redisson.getLock("lock");
+//
+//        try {
+//
+//            lock.lock();
+//
+//            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
+//            if (stock > 0) {
+//                int realStock = stock - 1;
+//                stringRedisTemplate.opsForValue().set("stock", realStock + "");
+//                System.out.println("扣减成功，剩余库存：" + realStock);
+//            } else {
+//                System.out.println("扣减失败，库存不足");
+//            }
+//        } finally {
+//            lock.unlock();
+//        }
+//
+//
+//        return "end";
+//    }
 }
