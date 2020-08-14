4fcb199cbfc413c15d770f08ffdb24dfa3f2c721
ym <yangming21_ok@13.com>
2020-08-14 17:30:28 +0800

    二分查找代码升级
    查找的数组中有重复的元素如何进行查找

--- demo/src/main/java/com/ym/learn/demo/algorithm/find/BinarySearch.java
+++ demo/src/main/java/com/ym/learn/demo/algorithm/find/BinarySearch.java
@@ -1,5 +1,8 @@
 package com.ym.learn.demo.algorithm.find;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 二分查找必须在有序数组中使用
  * 如果是无需数组必须先转换为有序数组
@@ -17,7 +20,18 @@ public class BinarySearch {
     public static void main(String[] args) {
         int arr[] = {1,8,10,89,1000,1234};
         System.out.println(binary_search(arr,1001));
+        System.out.println(binary_search_skr(arr,0,arr.length-1,1000));
+        int arr2[] = {1,1,8,10,89,1000,1000,1000,1234};
+        List<Integer> list = binary_search_improve(arr2, 0, arr2.length - 1, 1);
+        System.out.println(list);
     }
+
+    /**
+     * 非递归
+     * @param arr
+     * @param findVal
+     * @return
+     */
     private static int binary_search(int arr[],int findVal){
         int left = 0;
         int right = arr.length-1;
@@ -34,5 +48,63 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    /**
+     * 采用递归的方式
+     * @return
+     */
+    private static int binary_search_skr(int arr[],int left,int right,int findVal){
+        if (left>right)
+        return -1;
+        int mid = left+(right-left)/2;
+        int midVal = arr[mid];
+        if (findVal>midVal){
+            return binary_search_skr(arr, mid+1, right, findVal);
+        }else if (findVal<midVal){
+            return binary_search_skr(arr, left, mid-1, findVal);
+        }else {
+            return mid;
+        }
+    }
+
+    /**
+     * 如果一个有序数组中有多个相同的值，该怎么处理
+     * 思路分析：
+     * 1.在找到mid值后不要马上返回
+     * 2.向mid索引值左边进行扫描，将满足条件的下标记录下来
+     * 3.向mid右边进行扫描，将满足条件的下标记录下来
+     * 4.将所有满足条件的下标进行返回
+     * @return
+     */
+    private static List<Integer> binary_search_improve(int arr[],int left,int right,int findVal){
+
+        if (left>right)
+            return new ArrayList<>();
+        int mid = left+(right-left)/2;
+        int midVal = arr[mid];
+        if (findVal>midVal){
+            return binary_search_improve(arr, mid+1, right, findVal);
+        }else if (findVal<midVal){
+            return binary_search_improve(arr, left, mid-1, findVal);
+        }else {
+            ArrayList<Integer> resList = new ArrayList<>();
+            int temp = mid -1;
+            while (true){
+                if (temp<0 || arr[temp] != findVal){
+                    break;
+                }
+                resList.add(temp--);
+            }
+            resList.add(mid);
+            temp = mid+1;
+            while (true) {
+                if (temp>arr.length-1 || arr[temp] != findVal){
+                    break;
+                }
+                resList.add(temp++);
+            }
+            return resList;
+        }
     }
 }
