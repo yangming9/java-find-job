f11d437ae733e3e0371f9f18bec10ab9c4f0a01d
ym <yangming21_ok@13.com>
2020-08-14 23:48:52 +0800

    分治算法----汉诺塔游戏实现

---
+++ demo/src/main/java/com/ym/learn/demo/algorithm/find/DivideConquer.java
@@ -0,0 +1,24 @@
+package com.ym.learn.demo.algorithm.find;
+
+/**
+ * 分治算法
+ */
+public class DivideConquer {
+    public static void main(String[] args) {
+        hanoiTower(5,'a','b','c');
+    }
+    private static void hanoiTower(int num,char a,char b,char c){
+        //如果只有一个盘
+        if (num == 1){
+            System.out.println("第1盘从"+a+"->"+c);
+        }else {
+            //如果n>=2 情况，我们总是可以看做是两个盘 1.最下面的盘 2.上面的所有盘
+            //1.先把最上面的所有盘A->B，移动过程会使用到c
+            hanoiTower(num-1,a,c,b);
+            //2.把最下边的盘A->C
+            System.out.println("第"+num+"个盘从"+a+"->"+c);
+            //3.把B塔所有盘从B->C,移动过程使用到a塔
+            hanoiTower(num-1,b,a,c);
+        }
+    }
+}
