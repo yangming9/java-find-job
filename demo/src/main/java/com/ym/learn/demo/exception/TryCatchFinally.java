8a951dc95630287dd9f75f56d9b6e1872e21a1be
ym <yangming21_ok@13.com>
2020-08-29 23:46:56 +0800

    update

--- demo/src/main/java/com/ym/learn/demo/exception/TryCatchFinally.java
+++ demo/src/main/java/com/ym/learn/demo/exception/TryCatchFinally.java
@@ -1,4 +1,24 @@
 package com.ym.learn.demo.exception;
 
+/**
+ * 会执行，在return前执行
+ */
 public class TryCatchFinally {
+    public static void main(String[] args) {
+        System.out.println(getInt());
+    }
+    private static int getInt(){
+        int a = 10;
+        int b = 10;
+        try {
+            System.out.println((a+b)/0);
+            a = 20;
+        }catch (ArithmeticException e){
+            System.out.println("catch");
+            return a-b;
+        }finally {
+            System.out.println("finally");
+        }
+        return a;
+    }
 }
